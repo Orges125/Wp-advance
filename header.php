65a442502c84f476b942bce92d90ebfa0ee4c26c
unknown <Student@digitalschool.local>
2026-01-19 20:39:30 +0100

    ndryshime

--- header.php
+++ header.php
@@ -6,7 +6,25 @@
     <title>Document</title>
     <?php
        wp_head();
-       ?>
+    ?>
 </head>
 <body>
-    
+
+   <?php
+
+   if(|is_page('landing-page'));?>
+
+   <section class="menu-area">
+     
+    <nav class="main-menu">
+      <button class="check-button">
+             <div class="menu-icon">
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+         </div>
+     </button>  
+           <?php wp_nav_menu(array('theme_location' => 'wp_devs_main_menu', 'depth' => 1)); ?> 
+   </nav>
+  </div>
+</section>
